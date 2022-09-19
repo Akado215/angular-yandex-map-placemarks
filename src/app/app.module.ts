96f4d82ab1d7ba060477a72ad1b15836ffdeb8d9
Alexandr Glushkovskiy <alexander@beckndevs.com>
2022-09-19 20:39:27 +0300

    init commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+
+import { AngularYandexMapsModule, YaConfig } from 'angular8-yandex-maps';
+
+import { ReactiveFormsModule } from '@angular/forms';
+
+const mapConfig: YaConfig = {
+  apikey: '',
+  lang: 'ru_RU'
+};
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularYandexMapsModule.forRoot(mapConfig),
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
